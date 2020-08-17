e101bc6c688cfc6241a1c29364845e1f16f663e4
Guilherme Camargo <guisfits@hotmail.com>
2020-08-16 22:28:36 -0300

    routing

---
+++ routing/Publisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type : ExchangeType.Direct);
+
+                var severity = (args.Length > 0) ? args[0] : "info";
+                var message = (args.Length > 1) ?
+                    string.Join(" ", args.Skip(1).ToArray()) :
+                    "Hello World!";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "direct_logs",
+                    routingKey: severity,
+                    basicProperties : null,
+                    body : body);
+
+                Console.WriteLine($" [x] Send {severity}:{message}");
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
